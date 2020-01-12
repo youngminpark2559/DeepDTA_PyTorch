cfd71edacbf8ef6fd15e6a15e4c52dd24eca26f3
youngminpark2559 <youngminpark2559@gmail.com>
2020-01-12 18:44:01 +0900

    Add instantiating sentence for dataset

--- prj_root/src/train_script/train_by_deepdta.py
+++ prj_root/src/train_script/train_by_deepdta.py
@@ -1,7 +1,15 @@
 from src.networks import network_deepdta as network_deepdta
+from src.utils_for_dataset import dataset_deepdta as dataset_deepdta
 
-
+# ================================================================================
 def train(args):
+
+  deepdta_dataset_obj=dataset_deepdta.DeepDTA_Dataset(args)
+  print("deepdta_dataset_obj",deepdta_dataset_obj)
+  
+
+
+
   deepdta_obj=network_deepdta.DeepDTA(args)
   print("deepdta_obj",deepdta_obj)
   afaf
