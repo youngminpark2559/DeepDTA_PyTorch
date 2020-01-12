c04e79aa029bfbaf0bb3abf2fe7c4bc54017643f
youngminpark2559 <youngminpark2559@gmail.com>
2020-01-12 18:56:33 +0900

    Add sentence for loading can_smile data and protein data

--- prj_root/src/utils_for_dataset/dataset_deepdta.py
+++ prj_root/src/utils_for_dataset/dataset_deepdta.py
@@ -1,4 +1,8 @@
+import json
 import csv
+import pickle
+import collections
+from collections import OrderedDict
 import numpy as np
 import pandas as pd
 from random import shuffle
@@ -40,66 +44,18 @@ length_of_unique_characters_for_iso_smiles=len(unique_characters_for_iso_smiles)
 # ================================================================================
 class DeepDTA_Dataset(data.Dataset):
   def __init__(self,args):
-    print("args",args)
 
-    # print("single_train_k",single_train_k)
-    # [['/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_blue.png\n'
-    #   '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_green.png\n'
-    #   '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_red.png\n'
-    #   '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_yellow.png\n']
-
-    # print("single_train_lbl_k",single_train_lbl_k)
-    # [['000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0' '7 1 2 0']
-    #  ['000a9596-bbc4-11e8-b2bc-ac1f6b6435d0' '5']
-    #  ['000c99ba-bba4-11e8-b2b9-ac1f6b6435d0' '1']
-    #  ...
-    #  ['ffe55eba-bbba-11e8-b2ba-ac1f6b6435d0' '5 0']
-    #  ['ffe61798-bbc3-11e8-b2bc-ac1f6b6435d0' '19 23']
-    #  ['ffe8cf0c-bba9-11e8-b2ba-ac1f6b6435d0' '18']]
-
-    # print("single_train_lbl_k",single_train_lbl_k[:,1])
-    # ['7 1 2 0' '5' '1' ... '5 0' '19 23' '18']
-
-    # set_of_labels=[list(map(int,one_label_set.split(" "))) for one_label_set in single_train_lbl_k[:,1]]
-    # print("set_of_labels",set_of_labels)
-    # [[7, 1, 2, 0],
-
-    # zipped=np.array(list(zip(single_train_k,single_train_lbl_k[:,1])))
-    # print("zipped",zipped)
-    # [[array(['/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_blue.png',
-    #          '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_green.png',
-    #          '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_red.png',
-    #          '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_yellow.png'],dtype='<U140')
-    #  '7 1 2 0']
+    fpath = args.dataset_path	
+    # print("fpath",fpath)
+    # ../Data/kiba
 
     # ================================================================================
-    # zipped_new=[[list(one_protein_pair[0]),one_protein_pair[1]] for one_protein_pair in zipped]
-    # print("zipped_new",zipped_new)
-    # [[['/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_blue.png',
-    #    '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_green.png',
-    #    '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_red.png',
-    #    '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/000a6c98-bb9b-11e8-b2b9-ac1f6b6435d0_yellow.png'],
-    #  '7 1 2 0'],
- 
-    # took_time_min 0:00:00.130517
-
-    # shuffle(zipped_new)
-    # print("zipped_new",zipped_new)
-    # [[['/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/e6e2c6c0-bbae-11e8-b2ba-ac1f6b6435d0_blue.png',
-    #    '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/e6e2c6c0-bbae-11e8-b2ba-ac1f6b6435d0_green.png',
-    #    '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/e6e2c6c0-bbae-11e8-b2ba-ac1f6b6435d0_red.png',
-    #    '/mnt/1T-5e7/mycodehtml/bio_health/Kaggle/human-protein-atlas-image-classification/Data/train/e6e2c6c0-bbae-11e8-b2ba-ac1f6b6435d0_yellow.png'],
-    #  '3 0'],
-
-    # self.trn_pairs=zipped_new
-
-    # ================================================================================
-    # instance of argument 
-    # self.args=args
-
-    # ================================================================================
-    # self.nb_trn_imgs=len(single_train_k)
-    # 20714
+    ligands = json.load(open(fpath+"/ligands_can.txt"), object_pairs_hook=OrderedDict)
+    proteins = json.load(open(fpath+"/proteins.txt"), object_pairs_hook=OrderedDict)
+    # print("ligands",ligands)
+    # print("proteins",proteins)
+    # OrderedDict([('CHEMBL1087421', 'COC1=C(C=C2C(=C1)CCN=C2C3=CC(=C(C=C3)Cl)Cl)Cl'), ('CHEMBL1088633', 'COC1=C(C=C2C(=C1)CCN=C2C3=CC(=CC=C3)Cl)Cl'),
+    # proteins OrderedDict([('O00141', 'MTVKTEAAKGTLTYSRMRGMVAILIAFMKQRRMGLNDFIQKIANNSYACKHPEVQSILKISQPQEPELMNANPSPPPSPSQQINLGPSSNPHAKPSDFHFLKVIGKGSFGKVLLARHKAEEVFYAVKVLQKKAILKKKEEKHIMSERNVLLKNVKHPFLVGLHFSFQTADKLYFVLDYINGGELFYHLQRERCFLEPRARFYAAEIASALGYLHSLNIVYRDLKPENILLDSQGHIVLTDFGLCKENIEHNSTTSTFCGTPEYLAPEVLHKQPYDRTVDWWCLGAVLYEMLYGLPPFYSRNTAEMYDNILNKPLQLKPNITNSARHLLEGLLQKDRTKRLGAKDDFMEIKSHVFFSLINWDDLINKKITPPFNPNVSGPNDLRHFDPEFTEEPVPNSIGKSPDSVLVTASVKEAAEAFLGFSYAPPTDSFL'), ('O00311', 'MEASLGIQMDEPMAFSPQRDRFQAEGSLKKNEQNFKLAGVKKDIEKLYEAVPQLSNVFKIEDKIGEGTFSSVYLATAQLQVGPEEKIALKHLIPTSHPIRIAAELQCLTVAGGQDNVMGVKYCFRKNDHVVIAMPYLEHESFLDILNSLSFQEVREYMLNLFKALKRIHQFGIVHRDVKPSNFLYNRRLKKYALVDFGLAQGTHDTKIELLKFVQSEAQQERCSQNKSHIITGNKIPLSGPVPKELDQQSTTKASVKRPYTNAQIQIKQGKDGKEGSVGLSVQRSVFGERNFNIHSSISHESPAVKLMKQSKTVDVLSRKLATKKKAISTKVMNSAVMRKTASSCPASLTCDCYATDKVCSICLSRRQQVAPRAGTPGFRAPEVLTKCPNQTTAIDMWSAGVIFLSLLSGRYPFYKASDDLTALAQIMTIRGSRETIQAAKTFGKSILCSKEVPAQDLRKLCERLRGMDSSTPKLTSDIQGHASHQPAISEKTDHKASCLVQTPPGQYSGNSFKKGDSNSCEHCFDEYNTNLEGWNEVPDEAYDLLDKLLDLNPASRITAEEALLHPFFKDMSL'), ('O00329', 'MPPGVDCPMEFWTKEENQSVVVDFLLPTGVYLNFPVSRNANLSTIKQLLWHRAQYEPLFHMLSGPEAYVFTCINQTAEQQELEDEQRRLCDVQPFLPVLRLVAREGDRVKKLINSQISLLIGKGLHEFDSLCDPEVNDFRAKMCQFCEEAAARRQQLGWEAWLQYSFPLQLEPSAQTWGPGTLRLPNRALLVNVKFEGSEESFTFQVSTKDVPLALMACALRKKATVFRQPLVEQPEDYTLQVNGRHEYLYGSYPLCQFQYICSCLHSGLTPHLTMVHSSSILAMRDEQSNPAPQVQKPRAKPPPIPAKKPSSVSLWSLEQPFRIELIQGSKVNADERMKLVVQAGLFHGNEMLCKTVSSSEVSVCSEPVWKQRLEFDINICDLPRMARLCFALYAVIEKAKKARSTKKKSKKADCPIAWANLMLFDYKDQLKTGERCLYMWPSVPDEKGELLNPTGTVRSNPNTDSAAALLICLPEVAPHPVYYPALEKILELGRHSECVHVTEEEQLQLREILERRGSGELYEHEKDLVWKLRHEVQEHFPEALARLLLVTKWNKHEDVAQMLYLLCSWPELPVLSALELLDFSFPDCHVGSFAIKSLRKLTDDELFQYLLQLVQVLKYESYLDCELTKFLLDRALANRKIGHFLFWHLRSEMHVPSVALRFGLILEAYCRGSTHHMKVLMKQGEALSKLKALNDFVKLSSQKTPKPQTKELMHLCMRQEAYLEALSHLQSPLDPSTLLAEVCVEQCTFMDSKMKPLWIMYSNEEAGSGGSVGIIFKNGDDLRQDMLTLQMIQLMDVLWKQEGLDLRMTPYGCLPTGDRTGLIEVVLRSDTIANIQLNKSNMAATAAFNKDALLNWLKSKNPGEALDRAIEEFTLSCAGYCVATYVLGIGDRHSDNIMIRESGQLFHIDFGHFLGNFKTKFGINRERVPFILTYDFVHVIQQGKTNNSEKFERFRGYCERAYTILRRHGLLFLHLFALMRAAGLPELSCSKDIQYLKDSLALGKTEEEALKHFRVKFNEALRESWKTKVNWLAHNVSKDNRQ'),
 
   # ================================================================================
   def __len__(self):
