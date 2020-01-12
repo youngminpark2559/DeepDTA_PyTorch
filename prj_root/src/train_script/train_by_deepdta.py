84dc3ef817c48fe84b9667755475fcb057165655
youngminpark2559 <youngminpark2559@gmail.com>
2020-01-12 18:18:06 +0900

    Edit train_by_deepdta.py

--- prj_root/src/train_script/train_by_deepdta.py
+++ prj_root/src/train_script/train_by_deepdta.py
@@ -1,3 +1,7 @@
+from src.networks import network_deepdta as network_deepdta
+
 
 def train(args):
-  print("fffffffffff")
+  deepdta_obj=network_deepdta.DeepDTA(args)
+  print("deepdta_obj",deepdta_obj)
+  afaf
