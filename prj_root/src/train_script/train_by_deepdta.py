66c1ec16b857f3602f5ade937d41eeacee6f069e
youngminpark2559 <youngminpark2559@gmail.com>
2020-01-12 19:29:39 +0900

    Remove useless breaklines

--- prj_root/src/train_script/train_by_deepdta.py
+++ prj_root/src/train_script/train_by_deepdta.py
@@ -7,9 +7,6 @@ def train(args):
   deepdta_dataset_obj=dataset_deepdta.DeepDTA_Dataset(args)
   print("deepdta_dataset_obj",deepdta_dataset_obj)
   
-
-
-
   deepdta_obj=network_deepdta.DeepDTA(args)
   print("deepdta_obj",deepdta_obj)
   afaf
