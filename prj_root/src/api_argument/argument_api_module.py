72ff2001c4dca6be498b85bc08451a0f4d61d4f1
youngminpark2559 <youngminpark2559@gmail.com>
2020-01-12 17:27:34 +0900

    Add argument: embedding_vector_dimension_for_smiles_and_protein

--- prj_root/src/api_argument/argument_api_module.py
+++ prj_root/src/api_argument/argument_api_module.py
@@ -11,5 +11,6 @@ class Argument_API_class(argparse.ArgumentParser):
     self.add_argument("--train_method",help="train_by_custom_net,train_by_transfer_learning_using_resnet")
     self.add_argument("--max_length_of_smiles_sequence",default=100,help="integer")
     self.add_argument("--max_length_of_protein_sequence",default=1000,help="integer")
+    self.add_argument("--embedding_vector_dimension_for_smiles_and_protein",default=128,help="integer")
 
     self.args=self.parse_args()
